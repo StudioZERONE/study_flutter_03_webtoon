dcddc6eeb3755ab8077458594ae5974443aeb6d3
ZERONE <zerone012@gmail.com>
2023-02-03 10:16:03 +0900

    네이버 웹툰 API 호출. http 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:study_flutter_03_webtoon/screens/home_screen.dart';
+import 'package:study_flutter_03_webtoon/services/api_service.dart';
 
 void main() {
+  ApiService().getTodaysToons();
   runApp(const App());
 }
 
